cae228e264cb8f14d8219e824e397d0b3e457516
CoutinhoElias <coutinho.elias@gmail.com>
2016-09-08 13:27:37 -0300

    Filtrando Categorias com erro.

--- vitrinebela/catalog/models.py
+++ vitrinebela/catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -17,6 +18,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('catalog:category', kwargs={'slug': self.slug})
 
 
 class Product(models.Model):
--- vitrinebela/catalog/views.py
+++ vitrinebela/catalog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from.models import Product
+from.models import Product, Category
 
 
 def produtct_list(request):
@@ -12,3 +12,12 @@ def produtct_list(request):
         'product_list': Product.objects.all()
     }
     return render(request, 'catalog/product_list.html', context)
+
+
+def category_list(request, slug):
+    category = Category.objects.get(slug=slug)
+    context = {
+        'current_category': category,
+        'product_list': Product.objects.filter(category=category),
+    }
+    return render(request, 'catalog/category_list.html', context)
