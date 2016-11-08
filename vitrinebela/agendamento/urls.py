56a4c5cd13ea212c3dcc2ffa0c6aa7e943767ff9
Fábio C. Barrionuevo da Luz <bnafta@gmail.com>
2016-11-08 17:07:32 -0300

    Update urls.py

--- vitrinebela/agendamento/urls.py
+++ vitrinebela/agendamento/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
+from . import views
 
 urlpatterns = [
-    url(r'^$', 'agendamento.views.index', name='index'),
-    url(r'^all_events/', 'agendamento.views.all_events', name='all_events'),
+    url(r'^$', views.index, name='index'),
+    url(r'^all_events/', views.all_events, name='all_events'),
     # url(r'^admin/', include(admin.site.urls)),
 ]
