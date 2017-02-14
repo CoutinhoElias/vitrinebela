413529213e66a68a1534d0aa20c4b98f6aec19a5
Elias Coutinho <coutinho.elias@gmail.com>
2017-02-14 18:15:53 -0300

    Cadastro de usuarios na app accounts fora do admin

--- vitrinebela/accounts/forms.py
+++ vitrinebela/accounts/forms.py
@@ -2,6 +2,11 @@
 
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from material import Fieldset
+from material import Layout
+from material import Row
+from material import Span12
+from material import Span6
 
 from .models import User
 
@@ -11,6 +16,9 @@ class UserAdminCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+    layout = Layout(
+        Span6('username'), Span6('email'),
+        Row(Span6('password1'), Span6('password2')))
 
 
 class UserAdminForm(forms.ModelForm):
@@ -18,3 +26,4 @@ class UserAdminForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'name', 'is_active', 'is_staff']
+
