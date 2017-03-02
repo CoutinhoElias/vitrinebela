6a53b47cf128f6c1e27ee464b5a5f9ec40559bfb
Elias Coutinho <coutinho.elias@gmail.com>
2017-03-02 14:51:38 -0300

    Testando s3utils.py nova alteração

--- vitrinebela/s3utils.py
+++ vitrinebela/s3utils.py
@@ -1,20 +1,20 @@
 from storages.backends.s3boto import S3BotoStorage
 
-class StaticS3BotoStorage(S3BotoStorage):
+class StaticRootS3BotoStorage(S3BotoStorage):
     """
     Storage for static files.
     """
 
     def __init__(self, *args, **kwargs):
         kwargs['location'] = 'static'
-        super(StaticS3BotoStorage, self).__init__(*args, **kwargs)
+        super(StaticRootS3BotoStorage, self).__init__(*args, **kwargs)
 
 
-class MediaS3BotoStorage(S3BotoStorage):
+class MediaRootS3BotoStorage(S3BotoStorage):
     """
     Storage for uploaded media files.
     """
 
     def __init__(self, *args, **kwargs):
         kwargs['location'] = 'media'
-        super(MediaS3BotoStorage, self).__init__(*args, **kwargs)
+        super(MediaRootS3BotoStorage, self).__init__(*args, **kwargs)
